4c231da608f3c0bfba2b374be8cef1f04b02ce2c
Bodeh Sedrick <sedrickbodeh654@gamil.com>
2026-01-13 04:15:10 +0100

    Add manifest.json, service-coworker.js and adjust html for Progressive Web App (PWA)

---
+++ staticfiles/service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "uba-map-v1";
+
+const ASSETS = [
+  "/",
+  "/manifest.json",
+  "/service-worker.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
