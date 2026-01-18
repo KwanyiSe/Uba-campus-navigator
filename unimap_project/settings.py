7db0946a84b124aeb9e62489a96e4cde1f608986
Bodeh Sedrick <sedrickbodeh654@gamil.com>
2026-01-18 01:32:19 +0100

    stabel commit remove google analytics

--- unimap_project/settings.py
+++ unimap_project/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = 'django-insecure-1!h^&z*5@edx40&4-tp0=9dg0v@$yhl!5!l60y#+*6)mxkw7hd
 ORS_KEY = "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjEzYWUxMmU0NWIzNTQ1NzI5N2NhY2NjNWUyOGYyNGU0IiwiaCI6Im11cm11cjY0In0="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['Uba-navigator.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','Uba-navigator.onrender.com']
 
 #enable media file saving(photos/icons)
 MEDIA_URL = '/media/'
